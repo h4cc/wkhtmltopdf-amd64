4e96b688920a453196aa9522e28f6a25378325f8
markrosario <mark.anthony.r.rosario@gmail.com>
2023-03-31 16:20:42 +0800

    31 - add arm64 version of WKHTMLToPDF

--- WKHTMLToPDF.php
+++ WKHTMLToPDF.php
@@ -4,4 +4,12 @@ namespace h4cc\WKHTMLToPDF;
 class WKHTMLToPDF
 {
     const PATH = __DIR__ . '/bin/wkhtmltopdf-amd64';
+    const ARM64_PATH = __DIR__ . '/bin/wkhtmltopdf-arm64';
+
+    public static function getBinaryPath() : String {
+        if (php_uname('m') == 'aarch64' || php_uname('m') == 'arm64' ) {
+            return self::ARM64_PATH;
+        }
+        return self::PATH;
+    }
 }
